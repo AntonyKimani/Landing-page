cec99e263083f462ca632f0a486b2d9320899e86
Kimaniantony <kimaniantoni@gmail.com>
2021-12-28 08:13:54 +0300

    first push

---
+++ app.js
@@ -0,0 +1,11 @@
+const glass = document.querySelector(".glass-bar");
+const glassToggle = document.querySelector(".glass-bar h4");
+const glassClose = document.querySelector(".glass-bar h4");
+
+glass.addEventListener('click', ()=>{
+    
+})  
+
+function openBar(){
+    document.body.
+}
